aaffe8d34d19f5fe32f1a6a1570e06824dada578
Isaac <isaacfrett2@gmail.com>
2023-02-16 15:43:37 -0500

    yes

--- Module8/src/main/java/com/example/module8/CardApplication.java
+++ Module8/src/main/java/com/example/module8/CardApplication.java
@@ -2,9 +2,11 @@ package com.example.module8;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.TilePane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.*;
@@ -19,33 +21,48 @@ public class CardApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Random 4 Cards");
+        Button button = new Button("Refresh");
 
-        Image card1 = getRandomCards(primaryStage);
-        Image card2 = getRandomCards(primaryStage);
-        Image card3 = getRandomCards(primaryStage);
-        Image card4 = getRandomCards(primaryStage);
 
+        Image card1 = getRandomCards();
+        Image card2 = getRandomCards();
+        Image card3 = getRandomCards();
+        Image card4 = getRandomCards();
 
         ImageView imageView1 = new ImageView(card1);
         ImageView imageView2 = new ImageView(card2);
         ImageView imageView3 = new ImageView(card3);
         ImageView imageView4 = new ImageView(card4);
-        TilePane tilePane = new TilePane();
 
-        tilePane.getChildren().add(imageView1);
-        tilePane.getChildren().add(imageView2);
-        tilePane.getChildren().add(imageView3);
-        tilePane.getChildren().add(imageView4);
+        // On click we change the 4 images used previously
+        button.setOnAction((actionEvent) -> {
+            imageView1.setImage(getRandomCards());
+            imageView2.setImage(getRandomCards());
+            imageView3.setImage(getRandomCards());
+            imageView4.setImage(getRandomCards());
+        });
 
-        Scene scene = new Scene(tilePane, 450, 300);
+        HBox hBox = new HBox();
+        BorderPane bp = new BorderPane();
+        bp.setCenter(hBox);
+
+        // setup our view
+        hBox.getChildren().add(imageView1);
+        hBox.getChildren().add(imageView2);
+        hBox.getChildren().add(imageView3);
+        hBox.getChildren().add(imageView4);
+        bp.setBottom(button);
+
+        Scene scene = new Scene(bp, 450, 300);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
     }
 
 
 
-    public static Image getRandomCards(Stage primaryStage) {
-
+    public static Image getRandomCards() {
         try {
             int image_number = (int) (Math.random() * 52) + 1;
             File input = new File("src/cards/" + image_number + ".png");
